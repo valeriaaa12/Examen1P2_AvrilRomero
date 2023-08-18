c03f8b98a107be34259dc328bb1d26753d44af0c
valeriaaa12 <valeriaromero1275@gmail.com>
2023-08-18 13:19:39 -0600

    primer commit

---
+++ src/examen1p2_avrilromero/Examen1P2_AvrilRomero.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen1p2_avrilromero;
+
+/**
+ *
+ * @author rodge
+ */
+public class Examen1P2_AvrilRomero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
